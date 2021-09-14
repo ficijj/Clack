a277b992de4eb032457b62e91ef6284d8eba21d3
Adonisscal <scaltsar@clarkson.edu>
2021-09-14 18:28:48 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
+    private String username;
+    private int type;
+
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
+
+
---
+++ src/MessageClackData.java
@@ -0,0 +1,16 @@
+public class MessageClackData extends ClackData{
+
+    //variables
+    private String message;
+
+    //constructors
+public MessageClackData(String username, String message, int type){
+    super(username, type);
+    this.message = message;
+}
+
+public MessageClackData(){
+    this("Anon", " ", 0);
+}
+
+}
