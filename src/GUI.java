b9fd07ddd032c68c1e226eef28a4c04e66a5785c
ficijj <ficijj@clarkson.edu>
2021-12-02 20:22:37 -0500

    more gui stuff

--- src/GUI.java
+++ src/GUI.java
@@ -1,9 +1,13 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
 
@@ -18,19 +22,39 @@ public class GUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         FlowPane root = new FlowPane();
-        TextField tf = new TextField();
+        root.setPadding(new Insets(20,20,20,20));
+        VBox innerVBox = new VBox();
+        HBox hBox = new HBox();
+        VBox outerVBox = new VBox();
+        TextField tfInput = new TextField("input area");
+        TextField tfOutput = new TextField("output area");
+        tfOutput.setEditable(false);
 
         EventHandler<KeyEvent> keyHandler = new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
                 if(event.getCode().equals(KeyCode.ENTER)){
-                    tf.setText("");
+                    tfInput.setText("");
                 }
             }
         };
-        tf.setOnKeyPressed(keyHandler);
+        tfInput.setOnKeyPressed(keyHandler);
 
-        root.getChildren().add(tf);
+        root.setAlignment(Pos.BOTTOM_CENTER);
+        tfOutput.setAlignment(Pos.BOTTOM_LEFT);
+
+        tfOutput.setMinSize(400, 300);
+        tfOutput.setMaxSize(5000,5000);
+
+        tfInput.setMinSize(400, 30);
+        tfInput.setMaxSize(5000,30);
+
+        hBox.getChildren().add(tfInput);
+        innerVBox.getChildren().add(tfOutput);
+        outerVBox.getChildren().add(innerVBox);
+        outerVBox.getChildren().add(hBox);
+        root.getChildren().add(outerVBox);
+
         primaryStage.setScene( new Scene(root, 500, 500));
         primaryStage.show();
     }
